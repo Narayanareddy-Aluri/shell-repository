6a1ead21781dac1d12fc810fbc6eff31875ce322
NarayanaReddy <alurinani30@.com>
2026-01-28 20:50:08 +0530

    conditions

--- 05-condition.sh
+++ 05-condition.sh
@@ -4,9 +4,9 @@ Number=$1
 
 if [ $Number -gt 50 ]; then
 echo "Given number $Number greater than 50"
-elif [$Number -lt 50]; then
+elif [ $Number -lt 50 ]; then
 echo  "Given numbe $Number less than 50"
-elif [$Number -eq 50]; then
+elif [ $Number -eq 50 ]; then
 echo  "Given number $Number equal to 50"
 else
 echo  "Given number $Number not equal to 50"
